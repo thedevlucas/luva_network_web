9a79752854d8e047581439bf02defa2d05d7e34b
Fedeerich <marcosgongora1000@gmail.com>
2026-01-28 20:27:03 +0100

    Sitemap actualizado

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,6 +17,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/news/apertura-del-servidor-muy-pronto-web-oficial-inaugurada`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/store`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
